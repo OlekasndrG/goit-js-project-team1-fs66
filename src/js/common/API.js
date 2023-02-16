e1a9bd84c35b3a48fe3c3fa48c5ae8f924b8eb06
Olekasndr Gubskiy <infernokgg@gmail.com>
2023-02-17 01:44:24 +0200

    commonAPI
    commonAPI

---
+++ src/js/common/API.js
@@ -0,0 +1,27 @@
+const ENDPOINT = '';
+const API_KEY = 'lN1jAQVvVGOPSqcIQoMHMLLJA9oE1Rka';
+import Notiflix from 'notiflix';
+import axios from 'axios';
+
+export default class Picture {
+  constructor() {
+    this.name = '';
+    this.page = 1;
+  }
+
+  async articleSearchByQuery() {
+    const response = await axios(
+      `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${this.name}&api-key=${API_KEY}`
+    );
+    // https://api.nytimes.com/svc/search/v2/articlesearch.json?q=ukraine&api-key=eQ8t8FWqeAGnKDTtIFrHmgZCflFrUTcV
+
+    return response.data.response.docs;
+  }
+  resetPage() {
+    this.page = 1;
+  }
+
+  incrementPage() {
+    this.page += 1;
+  }
+}
