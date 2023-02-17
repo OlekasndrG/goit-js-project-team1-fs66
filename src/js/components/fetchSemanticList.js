9c548682f647dc8b57e67ed5710af5d70416359c
Kornilov <110562214+Dimar95@users.noreply.github.com>
2023-02-17 16:46:23 +0200

    fix

--- src/js/components/fetchSemanticList.js
+++ src/js/components/fetchSemanticList.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+
 export async function fetchCategories() {
   try {
     const BASE_URL = 'https://api.nytimes.com/svc//news/v3/content/section-list.json?api-key='
@@ -6,6 +7,7 @@ export async function fetchCategories() {
     const data = await axios.get(`${BASE_URL}${KEY}`);
     
     return data.data.results
+
   } catch (error) {
   console.error(error);
 }
