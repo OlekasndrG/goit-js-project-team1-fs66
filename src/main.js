7b6b05a9bf5416cc7bb3ebb67f25aa5f36b9ec16
Olekasndr Gubskiy <infernokgg@gmail.com>
2023-02-19 21:55:50 +0200

    minor

--- src/main.js
+++ src/main.js
@@ -11,5 +11,8 @@ import './js/components/burger-menu';
 import './js/components/dataBase/auth';
 import './js/components/dataBase/setDatabase';
 import './js/components/dataBase/register';
-import './js/components/dataBase/modal'
-import './js/components/dataBase/authForm'
+import './js/components/dataBase/modal';
+import './js/components/dataBase/authForm';
+import './js/components/input';
+import './js/common/markup_template';
+import './js/common/weather_API';
