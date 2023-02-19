98f7a27f955f750a906d100d685fb88d240e0cbc
RomahaVO <romahabodnaruk@icloud.com>
2023-02-19 21:50:47 +0200

    fixedGetPopularArticle

--- src/js/components/articles.js
+++ src/js/components/articles.js
@@ -1,15 +1,15 @@
 const KEY = 'lN1jAQVvVGOPSqcIQoMHMLLJA9oE1Rka';
 const MOST_POPULAR_NEWS = `https://api.nytimes.com/svc/mostpopular/v2/viewed/1.json?api-key=${KEY}`;
 
-const popularArticleArr = [];
-// console.log(popularArticleArr);
+
 
 async function getPopularArticle() {
     const articleFetch = await fetch(MOST_POPULAR_NEWS);
     const articles = await articleFetch.json();
     let { results } = articles;
-    console.log(results);
-    return results.map(({id, media, title, published_date, abstract, url, section})=>`<li class='articles-list-item' id='${id}'>
+    // console.log(results);
+    
+    const articlesCardsArr = results.map(({id, media, title, published_date, abstract, url, section})=>`<li class='articles-list-item' id='${id}'>
     <article class='articles-item-news'>
 
             <div class='articles-news-wrapper'>
@@ -34,9 +34,14 @@ async function getPopularArticle() {
                 .replaceAll('-', '/')}</span>
                 <a target='blank' class='articles-link-news'  href='${url}'>Read more</a>
             </div>
-
     </article>
-</li>`).join();
+</li>`).join(' '); 
 
+updateNewArticles(articlesCardsArr);
 };
-getPopularArticle(popularArticleArr);
+
+getPopularArticle();
+
+function updateNewArticles(articlesCardsArr) {
+    // document.querySelector('.country-list').innerHTML = articlesCardsArr;
+};
