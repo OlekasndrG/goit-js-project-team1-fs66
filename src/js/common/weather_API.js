a1f12c15ad5bb39aa784817055b00b2534534eb5
Olekasndr Gubskiy <infernokgg@gmail.com>
2023-02-19 14:07:07 +0200

    minor weather

--- src/js/common/weather_API.js
+++ src/js/common/weather_API.js
@@ -1,59 +1,42 @@
-import axios, { Axios } from "axios";
+import axios from 'axios';
 
-
-const ENDPOINT = "https://api.openweathermap.org/data/2.5/weather?";
-const API_KEY = "eb208d9e6f2962fbd904029d1ed12318";
-
-
+const ENDPOINT = 'https://api.openweathermap.org/data/2.5/weather?';
+const API_KEY = 'eb208d9e6f2962fbd904029d1ed12318';
 
 let geoData = null;
 
 getGeolocation();
 
-
-   
-
-function getGeolocation()
-{
-    navigator.geolocation.getCurrentPosition(geolocationSuccess, error);
-
-    
-    
+function getGeolocation() {
+  navigator.geolocation.getCurrentPosition(geolocationSuccess, error);
 }
 async function error() {
-    console.log('error');
-    console.log(await getWeather());
- }
+  console.log('error');
+  console.log(await getWeather());
+}
 
-async function geolocationSuccess(succeessGeo)
-{
-    geoData = succeessGeo;
-    
+async function geolocationSuccess(succeessGeo) {
+  geoData = succeessGeo;
 
-     console.log(await getWeather());
-   
-
+  console.log(await getWeather());
 }
 
 async function getWeather() {
-    
-    let latitude = 44.34;
-    let longitude = 10.99;
-  
-    
+  let latitude = 44.34;
+  let longitude = 10.99;
 
-    if (geoData !== null) {
-        latitude = geoData.coords.latitude;
-        longitude = geoData.coords.longitude;
-    }
-    else {
-        console.log("geo data = null")
-    }
+  if (geoData !== null) {
+    latitude = geoData.coords.latitude;
+    longitude = geoData.coords.longitude;
+  } else {
+    console.log('geo data = null');
+  }
 
-    const response = await axios.get(`${ENDPOINT}lat=${latitude}&lon=${longitude}&units=metric&lang=en&appid=${API_KEY}`);
+  const response = await axios.get(
+    `${ENDPOINT}lat=${latitude}&lon=${longitude}&units=metric&lang=en&appid=${API_KEY}`
+  );
 
-    return response;
-
+  return response;
 }
 
 export default { getWeather };
