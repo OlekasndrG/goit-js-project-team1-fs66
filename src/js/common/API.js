01ea03c083b68d1864134c40e65bd95874887532
Nataleesha <frojlan@gmail.com>
2023-02-20 05:03:09 +0200

    api and pagination

--- src/js/common/API.js
+++ src/js/common/API.js
@@ -1,26 +1,92 @@
 const ENDPOINT = '';
-const API_KEY = 'lN1jAQVvVGOPSqcIQoMHMLLJA9oE1Rka';
+const V2_API_KEY = 'lN1jAQVvVGOPSqcIQoMHMLLJA9oE1Rka';
+const V3_API_KEY = '3CItmRm8j68GAL0hCexL4TtbIz43N2Mu';
 import axios from 'axios';
+import { format } from 'date-fns';
 
-export default class API {
-  constructor() {
-    this.name = '';
-    this.page = 1;
-  }
+class API {
+  async articleSearchByQuery(q, page=1, date) {
+    let fq = '';
+    if (date) {
+      fq = `pub_date:${format(date, 'yyyy-MM-dd')}`;
+    }
 
-  async articleSearchByQuery() {
-    const response = await axios(
-      `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${this.name}&api-key=${API_KEY}`
+    const response = await axios.get(
+      'https://api.nytimes.com/svc/search/v2/articlesearch.json',
+      {
+        params: {
+          q: q,
+          page: page,
+          fq: fq,
+          'api-key': V2_API_KEY,
+        },
+      }
     );
-    // https://api.nytimes.com/svc/search/v2/articlesearch.json?q=ukraine&api-key=eQ8t8FWqeAGnKDTtIFrHmgZCflFrUTcV
 
-    return response.data.response.docs;
-  }
-  resetPage() {
-    this.page = 1;
+    const articles = response.data.response.docs.map(result => {
+      let image;
+      if (result.multimedia.length > 0) {
+        image = 'https://nyt.com/' + result.multimedia[0].url;
+      }
+
+      return {
+        title: result.headline.main,
+        image: image,
+        description: result.abstract,
+        date: result.pub_date,
+        url: result.web_url,
+      };
+    });
+
+    return {
+      articles: articles,
+      total: response.data.response.meta.hits,
+    };
   }
 
-  incrementPage() {
-    this.page += 1;
+  async articleSearchMostPopular() {
+    const response = await axios.get(
+      'https://api.nytimes.com/svc/mostpopular/v2/viewed/1.json',
+      { params: { 'api-key': V2_API_KEY } }
+    );
+
+    const articles = response.data.results.map(result => {
+      return {
+        title: result.title,
+        image: result.media[0]['media-metadata'][2].url,
+        description: result.abstract,
+        date: result.published_date,
+        url: result.url,
+      };
+    });
+
+    return {
+      articles: articles,
+      total: response.data.num_results,
+    };
+  }
+
+  async articleSearchByCategory(category, page = 1) {
+    const response = await axios(
+      `https://api.nytimes.com/svc/news/v3/content/all/${category}.json`,
+      { params: { 'api-key': V3_API_KEY, limit: 8, offset: page * 8 } }
+    );
+
+    const articles = response.data.results.map(result => {
+      return {
+        title: result.title,
+        image: result.multimedia[2].url,
+        description: result.abstract,
+        date: result.published_date,
+        url: result.url,
+      };
+    });
+
+    return {
+      articles: articles,
+      total: response.data.num_results,
+    };
   }
 }
+
+export default new API();
---
+++ src/js/components/pagination.js
@@ -0,0 +1,128 @@
+import api from '../common/API';
+
+class Paginator {
+  currentPage = 1;
+  itemsPerPage = 8;
+  totalPages = 1;
+  delta = 1;
+
+  ellipsisTemplate = `<li class='pg-item'><a class='pg-link'>...</a></li>`;
+
+  constructor(selector) {
+    this.rootEl = document.querySelector(selector);
+    this.pagesEl = this.rootEl.querySelector('.pages');
+    this.btnPrevPg = this.rootEl.querySelector('.prev-page');
+    this.btnNextPg = this.rootEl.querySelector('.next-page');
+    this.addEventListeners();
+  }
+
+  addEventListeners() {
+    this.rootEl.addEventListener('click', e => {
+      const el = e.target;
+      if (el.classList.contains('next-page')) {
+        this.setPage(this.currentPage + 1);
+      } else if (el.classList.contains('prev-page')) {
+        this.setPage(this.currentPage - 1);
+      } else if (el.classList.contains('pg-link')) {
+        const page = parseInt(el.dataset.page, 10);
+        this.setPage(page);
+      }
+    });
+  }
+
+  init(perPage, totalItems) {
+    this.itemsPerPage = perPage;
+    this.totalPages = Math.ceil(totalItems / perPage);
+    if (this.totalPages > 1) {
+      this.setPage(1);
+    } else {
+      this.rootEl.style.display = 'none';
+    }
+  }
+
+  setPage(page) {
+    if (page > this.totalPages) {
+      page = this.totalPages;
+    } else if (page < 1) {
+      page = 1;
+    }
+    this.currentPage = page;
+    this.render();
+    this.dispatch();
+  }
+
+  onPageChange(callback) {
+    this.rootEl.addEventListener('pageChanged', callback);
+  }
+
+  dispatch() {
+    const event = new Event('pageChanged');
+    this.rootEl.dispatchEvent(event);
+  }
+
+  getPageHtml(page, isActive) {
+    return `<li class='pg-item${
+      isActive ? ' active' : ''
+    }'><a class='pg-link' href='#' data-page='${page}'>${page}</a></li>`;
+  }
+
+  render() {
+    console.log(this.currentPage, this.totalPages);
+    const range = this.delta + 4;
+    let truncated = 0;
+    const truncatedLeft = this.currentPage - this.delta;
+    const truncatedRight = this.currentPage + this.delta;
+    let html = '';
+    let htmlWithEllipsis = '';
+    for (let i = 1; i <= this.totalPages; i++) {
+      const isActive = i === this.currentPage;
+      if (this.totalPages >= 2 * range - 1) {
+        if (truncatedLeft > 3 && truncatedRight < this.totalPages - 3 + 1) {
+          if (i >= truncatedLeft && i <= truncatedRight) {
+            htmlWithEllipsis += this.getPageHtml(i, isActive);
+          }
+        } else {
+          if (
+            (this.currentPage < range && i <= range) ||
+            (this.currentPage > this.totalPages - range &&
+              i >= this.totalPages - range + 1) ||
+            i === this.totalPages ||
+            i === 1
+          ) {
+            html += this.getPageHtml(i, isActive);
+          } else {
+            truncated++;
+            if (truncated === 1) {
+              html += this.ellipsisTemplate;
+            }
+          }
+        }
+      } else {
+        html += this.getPageHtml(i, isActive);
+      }
+    }
+    if (htmlWithEllipsis) {
+      this.pagesEl.innerHTML =
+        this.getPageHtml(1) +
+        this.ellipsisTemplate +
+        htmlWithEllipsis +
+        this.ellipsisTemplate +
+        this.getPageHtml(this.totalPages);
+      this.rootEl.style.display = 'block';
+    } else if (html) {
+      this.pagesEl.innerHTML = html;
+      this.rootEl.style.display = 'block';
+    } else {
+      this.rootEl.style.display = 'none';
+    }
+  }
+}
+
+const paginator = new Paginator('.pagination');
+
+async function renderArticles() {
+  const result = await api.articleSearchMostPopular();
+  console.log(result);
+}
+
+renderArticles();
