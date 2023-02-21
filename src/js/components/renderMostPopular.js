f5cd5ad1158da12e2c914e757c9d8bcf92c7fa2f
Olekasndr Gubskiy <infernokgg@gmail.com>
2023-02-22 00:10:35 +0200

    minor

--- src/js/components/renderMostPopular.js
+++ src/js/components/renderMostPopular.js
@@ -3,7 +3,7 @@ import api from '../common/API';
 import { findFavoriteCards, findReadCards } from './articles';
 import { onRenderOneCard } from './renderOneCard';
 import paginator from './pagination.js';
-import { findFavoriteCards, findReadCards } from './articles';
+
 onRanderMostPopular();
 async function onRanderMostPopular() {
   const result = await api.articleSearchMostPopular();
