2bf23f948b434d1a0a03971ca388fac55cec4463
Olekasndr Gubskiy <infernokgg@gmail.com>
2023-02-21 14:52:36 +0200

    minor

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,17 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
-
 export default defineConfig({
-  root: 'src',
+  root: '‘src',
   build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'src/index.html'),
+        second: resolve(__dirname, 'src/second.html'),
+        next: resolve(__dirname, 'src/next.html'),
+      },
+    },
     outDir: '../dist',
   },
   plugins: [injectHTML(), FullReload(['./src/**/**.html'])],
