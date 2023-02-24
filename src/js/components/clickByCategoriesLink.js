96eab8ee18958af17077fc0e422d1b2cd3edaf72
Olekasndr Gubskiy <infernokgg@gmail.com>
2023-02-25 01:01:32 +0200

    minor

--- src/js/components/clickByCategoriesLink.js
+++ src/js/components/clickByCategoriesLink.js
@@ -2,22 +2,23 @@ import { onRenderOneCard } from './renderOneCard';
 import api from '../common/API';
 import paginator from './pagination';
 const data = localStorage.getItem('Date_current');
-export let classActiv = null
+const inputEl = document.getElementById('search');
+console.log(inputEl);
+export let classActiv = null;
 
 export function onSerchByCategori() {
   const dataCurrent = localStorage.getItem('Date_current');
   const categoriesLink = document.querySelector('.categories-box');
   categoriesLink.addEventListener('click', e => {
-
     if (e.target.nodeName !== 'A') {
       return;
-    }   
-    if (classActiv !== null) {
-      classActiv.classList.remove('categories-activ')
     }
-    classActiv= e.target.parentNode
+    if (classActiv !== null) {
+      classActiv.classList.remove('categories-activ');
+    }
+    classActiv = e.target.parentNode;
     classActiv.classList.add('categories-activ');
-
+    inputEl.value = '';
     const category = encodeURIComponent(
       `${e.target.textContent.toLowerCase()}`
     );
